2f01dfbbfa44a66dc5e26991d364092425ebd9fe
Nina Sukonrat <144795388+ninaneens@users.noreply.github.com>
2023-11-28 18:23:28 +0100

    Update SpotifyTop50.py

--- SpotifyTop50.py
+++ SpotifyTop50.py
@@ -551,8 +551,7 @@ if app_mode == 'Prediction 🌠':
     def predict(target_choice, train_size, new_df,feature_choice):
         #independent variables / explanatory variables
         #choosing column for target
-        print(feature_choice)
-        new_df2 = new_df[[list(feature_choice)]]
+        new_df2 = new_df[feature_choice]
         x =  new_df2
         y = df[target_choice]
         col1,col2 = st.columns(2)
